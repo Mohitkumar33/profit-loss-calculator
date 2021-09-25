3c1d5fa1ea4dfaded057054c93920d03d6ba3b82
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2021-09-25 15:21:42 +0530

    completed basic html and javascript

---
+++ app.js
@@ -0,0 +1,48 @@
+const boughtPrice = document.querySelector("#bought-price");
+const quantityBought = document.querySelector("#quantity");
+const currentPrice = document.querySelector("#current-price");
+const checkButton = document.querySelector("#check-button");
+const output = document.querySelector("#text-area");
+
+output.style.display = "none";
+
+function calculate() {
+  var buyPrice = Number(boughtPrice.value);
+  var quantity = Number(quantityBought.value);
+  var current = Number(currentPrice.value);
+  if (buyPrice > 0 && quantity > 0 && current >= 0) {
+    if (buyPrice < current) {
+      var profit = current - buyPrice;
+      var profitPercentage = (profit * 100) / buyPrice;
+      output.style.display = "block";
+      output.innerText =
+        "Your profit is " +
+        profit * quantity +
+        " and the profit percentage is " +
+        profitPercentage +
+        "🎉✨";
+      output.style.color = "green";
+    } else if (buyPrice > current) {
+      var loss = buyPrice - current;
+      var lossPercentage = (loss * 100) / buyPrice;
+      output.style.display = "block";
+      output.innerText =
+        "Your loss is" +
+        loss * quantity +
+        " and the loss percentage is " +
+        lossPercentage +
+        "😐";
+      output.style.color = "red";
+    } else {
+      output.style.display = "block";
+      output.innerText = "No profit no loss";
+    }
+  } else {
+    output.style.display = "block";
+    output.innerText = "Enter a valid input";
+  }
+
+  //   console.log(buyPrice);
+}
+
+checkButton.addEventListener("click", calculate);
